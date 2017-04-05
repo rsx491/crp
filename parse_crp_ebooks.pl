baa718fb49ce746c38d03d0691ec03a7ca717248
rsx491 <ryan81s@gmail.com>
2017-04-05 12:36:18 -0400

    Update parse_crp_ebooks.pl

--- parse_crp_ebooks.pl
+++ parse_crp_ebooks.pl
@@ -21,8 +21,8 @@ my %attr = ( RaiseError=> 1,  # error handling enabled
 my $dsn = "DBI:mysql:database=crp;host=localhost;port=3306";
 my $dbh = DBI->connect(
 						$dsn, 
-						'root', 
-						'gunslinger',
+						'username', 
+						'password',
 						\%attr,
 ) or die $DBI::errstr;
 
